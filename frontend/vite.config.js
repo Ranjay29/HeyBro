6dbd31c28a9ab8184e994e481c1fb9c8c13bc6d7
Ranjay <gjranjay12@gmail.com>
2026-04-30 15:32:43 +0530

    files updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,12 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  build: {
+    outDir: 'docs', // Tells Vite to build into the 'docs' folder
+    emptyOutDir: true, // Cleans the folder before each build
+  },
+  // If your site is at username.github.io/HeyBro/, add this:
+  base: '/HeyBro/', 
 })
